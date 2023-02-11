7806a6ea94c621899f910abe4d38026ee00fb033
armelwanes <armellin230@gmail.com>
2023-02-11 11:58:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function createWindow () {
 
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
